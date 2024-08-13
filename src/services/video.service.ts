dbe84b6142bf760c5d49463ce3c31e543b8a4d27
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-08-13 19:01:55 +0530

    feat: video service added

---
+++ src/services/video.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class VideoSerice {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async uploadService(createVideoData, filename: string) {
+    return this.prisma.video.create({
+      data: {
+        ...createVideoData,
+        url: `/uploads/videos/${filename}`,
+      },
+    });
+  }
+
+  async findAll(id: number) {
+    return this.prisma.video.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.video.findUnique({
+      where: { id },
+    });
+  }
+}
