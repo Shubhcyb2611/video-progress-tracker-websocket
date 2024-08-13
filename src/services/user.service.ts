8569ef07535d20782161dd6311a4ad8d854e8a2d
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-08-13 18:39:32 +0530

    feat : User service added

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,4 +4,16 @@ import { PrismaService } from 'src/prisma.service';
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(email: string, password: string) {
+    return this.prisma.user.create({
+      data: { email, password },
+    });
+  }
+
+  async findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email: email },
+    });
+  }
 }
