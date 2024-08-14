ca8d190ce2b41d2739ff7bf439ddafcda6589883
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-08-14 11:45:43 +0530

    feat/users added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyStatic from '@fastify/static';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Create the NestJS application with Fastify adapter
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // Enable CORS if needed
+  app.enableCors();
+
+  // Register Fastify static plugin to serve files
+  app.register(fastifyStatic, {
+    root: join(__dirname, '..', 'uploads'),
+    prefix: '/uploads/', // URL prefix for accessing uploaded files
+  });
+
+  // Start listening for incoming connections on port 3000
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
