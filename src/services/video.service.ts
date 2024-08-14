9a1eaf5788821aadd13e46b297e390d558b0196f
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-08-14 13:58:47 +0530

    feat : composite unique constraint added

--- src/services/video.service.ts
+++ src/services/video.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { VideoGateway } from 'src/gateway/video.gateway';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class VideoService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly videoGateway: VideoGateway,
+  ) {}
 
   async uploadVideo(createVideoData, filename: string, userId) {
     return this.prisma.video.create({
@@ -27,6 +31,15 @@ export class VideoService {
   async findOne(id: number) {
     return this.prisma.video.findUnique({
       where: { id },
+    });
+  }
+
+  //user progress
+  async updateProgress(userId: number, videoId: number, progress: number) {
+    await this.prisma.userProgress.upsert({
+      where: { AND: [{ userId: userId }, { videoId: videoId }] },
+      update: {},
+      create: {},
     });
   }
 }
